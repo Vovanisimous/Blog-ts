4897719426b07b7e13a3ebb7ec5b15a15f2f4bb4
Vovanisimous <vovanisimous@yandex.com>
2020-08-19 12:07:22 +0300

    changed avatarName to FileName in useStorage

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,24 +1,24 @@
 import { useFirebase } from "./useFirebase";
 
 interface IFirebaseStorage<T> {
-    getDownloadURL(avatarName: string): Promise<any>;
-    getChildDownloadURL(avatarName: string): Promise<any>;
+    getDownloadURL(fileName: string): Promise<any>;
+    getChildDownloadURL(fileName: string): Promise<any>;
     put(data: Blob | Uint8Array | ArrayBuffer, avatarName: string): firebase.storage.UploadTask;
 }
 
 export const useStorage = <T>(): IFirebaseStorage<T> => {
     const { storage } = useFirebase();
 
-    const getDownloadURL = (avatarName:string) => {
-        return storage().ref(avatarName).getDownloadURL()
+    const getDownloadURL = (fileName:string) => {
+        return storage().ref(fileName).getDownloadURL()
     }
 
-    const getChildDownloadURL = (avatarName:string) => {
-        return storage().ref().child(avatarName).getDownloadURL()
+    const getChildDownloadURL = (fileName:string) => {
+        return storage().ref().child(fileName).getDownloadURL()
     }
 
-    const put = (data: Blob | Uint8Array | ArrayBuffer, avatarName: string) => {
-        return storage().ref().child(avatarName).put(data)
+    const put = (data: Blob | Uint8Array | ArrayBuffer, fileName: string) => {
+        return storage().ref().child(fileName).put(data)
     }
 
     return {
