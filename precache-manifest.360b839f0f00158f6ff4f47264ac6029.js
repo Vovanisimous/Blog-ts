ad894cd650f65edd88758e1af44b27089285f5c0
Vovanisimous <vovanisimous@yandex.com>
2020-08-28 11:48:14 +0300

    Updates

---
+++ precache-manifest.360b839f0f00158f6ff4f47264ac6029.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "198b14e19d75ce5d3e3b5c1c908dc3a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c740b4c7c4d6db83344d",
+    "url": "/static/css/main.4941300d.chunk.css"
+  },
+  {
+    "revision": "885a04a87ee62d574375",
+    "url": "/static/js/2.bfae6822.chunk.js"
+  },
+  {
+    "revision": "c273eb4a785b293b1d9921dc5551d18e",
+    "url": "/static/js/2.bfae6822.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c740b4c7c4d6db83344d",
+    "url": "/static/js/main.dd139a4b.chunk.js"
+  },
+  {
+    "revision": "1209add7631694ddf8b1",
+    "url": "/static/js/runtime-main.3dc933a3.js"
+  },
+  {
+    "revision": "5097b0247a92d47178df598b82944f15",
+    "url": "/static/media/default-avatar.5097b024.png"
+  }
+]);
--- publish.js
+++
@@ -1,6 +0,0 @@
-const ghpages = require('gh-pages');
-
-ghpages.publish("build", {
-    branch: "deploy",
-    repo: "https://github.com/Vovanisimous/Blog-ts.git"
-}, (msg) => console.log(msg));
